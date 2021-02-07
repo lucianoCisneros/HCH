f00c5206c0cedd5fe31c62e9bba4009df5c03259
Luciano Cisneros <lucianocisneros22@gmail.com>
2021-02-07 18:48:42 -0300

    Primer commit

---
+++ public/javascripts/navBar.js
@@ -0,0 +1,10 @@
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.menu-btn');
+    if(btn_menu){
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu-items')
+            menu_items.classList.toggle('show')
+        }
+        )
+    }
+})
